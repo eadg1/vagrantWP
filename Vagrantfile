cda50298893a1b8f198b620d93def370217914f2
anna <anna@www.trueheart.com>
2020-11-17 10:55:26 +0200

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,49 @@
+require 'yaml'
+
+config_file = YAML.load_file("config.yml")
+settings = config_file['env'];
+
+Vagrant.configure(2) do |config|
+
+  # vagrant_domain = "onix.kr.ua.local"
+  vagrant_ip = settings['vagrant_network'] + rand(256).to_s
+  vagrant_sitename=settings['WPSITENAME']
+  config.vm.box = "ubuntu/focal64"
+
+  config.vm.network "private_network", ip: "#{vagrant_ip}"
+  config.vm.hostname = settings['vagrant_domain']
+  config.hostsupdater.aliases = ["mail.#{settings['vagrant_domain']}", "db.#{settings['vagrant_domain']}"]
+
+  config.vm.synced_folder settings['SITE_FOLDER'], "/srv/www/",  type: "nfs", mount_options:["soft", "nolock"],  :linux__nfs_options => ["rw"]
+  config.vm.synced_folder settings['LOGS_FOLDER'], "/srv/log/", type: "nfs", mount_options:["soft", "nolock"],  :linux__nfs_options => ["rw"]
+  config.vm.synced_folder "config/", "/srv/config", type: "nfs", mount_options:["soft", "nolock"],  :linux__nfs_options => ["rw"]
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui  = false
+    vb.name = vagrant_sitename
+    vb.customize ["modifyvm", :id, "--memory", 512]
+    vb.customize ["modifyvm", :id, "--cpus", 1]
+    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+  end
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
+  config.vm.provision "shell", path: "provision.sh", env: { 
+                                                            DBROOTPASS:settings['DBROOTPASS'], 
+                                                            DBPREFIX:settings['DBPREFIX'],
+                                                            DBNAME:settings['DBNAME'],
+                                                            DBUSER:settings['DBUSER'],
+                                                            DBPASS:settings['DBPASS'],
+                                                            WPURL:settings['WPURL'],
+                                                            WPHOST:settings['WPHOST'],
+                                                            WPSITENAME:settings['WPSITENAME'],
+                                                            WPADMUSER:settings['WPADMUSER'],
+                                                            WPADMPASS:settings['WPADMPASS'],
+                                                            WPADMEMAIL:settings['WPADMEMAIL'],
+                                                            WP_VERSION:settings['WP_VERSION'],
+                                                            GIT_REPO:settings['GIT_REPO'],
+                                                            GIT_PASS:settings['GIT_PASS'],
+                                                            GIT_USER:settings['GIT_USER'],
+                                                            DB_DUMP: settings ['DB_DUMP']
+                                                          },
+                                                        args: "#{settings['vagrant_domain']}"
+end
---
+++ provision.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+# Set start time
+time_start="$(date +%s)"
+
+# Get domain name passed from Vagrantfile
+vagrant_domain=$1
+
+
+# Set MySQL default root password
+echo "Setting up root MySQL password..."
+echo mariadb-server mysql-server/root_password password password | debconf-set-selections
+echo mariadb-server mysql-server/root_password_again password password | debconf-set-selections
+
+# phpMyAdmin unattended installation
+echo "Setting up default phpMyAdmin configuration..."
+echo phpmyadmin phpmyadmin/dbconfig-install boolean true | debconf-set-selections
+echo phpmyadmin phpmyadmin/app-password-confirm password password | debconf-set-selections
+echo phpmyadmin phpmyadmin/mysql/admin-pass password password | debconf-set-selections
+echo phpmyadmin phpmyadmin/mysql/app-pass password password | debconf-set-selections
+echo phpmyadmin phpmyadmin/reconfigure-webserver multiselect none | debconf-set-selections
+
+echo "Updating packages list..."
+apt-get update && \
+apt-get upgrade -y && \
+apt-get install -yqq \
+    build-essential \
+    git \
+    imagemagick \
+    mariadb-server\
+    nginx \
+    nodejs \
+    npm \
+    ntp \
+    php-curl \
+    php-fpm \
+    php-dev \
+    php-gd \
+    php-imap \
+    php-mbstring \
+    php-mysql \
+    php-soap \
+    php-xml \
+    php-xmlrpc \
+    php-zip \
+    php-imagick \
+    php-pear \
+    subversion \
+    unzip \
+    zip
+
+# Install phpmyadmin separately
+apt-get install -y phpmyadmin
+
+# Install Composer
+if [ ! -f /usr/local/bin/composer ]; then
+    echo "Installing Composer..."
+    curl -sS https://getcomposer.org/installer | php
+    chmod +x composer.phar
+    mv composer.phar /usr/local/bin/composer
+fi
+
+# Install WP-CLI
+if [ ! -f /usr/local/bin/wp ]; then
+    echo "Installing wp-cli..."
+    curl -sS -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
+
+
+
+
+# Install mailhog
+echo "Installing mailhog..."
+wget --quiet -O ~/mailhog https://github.com/mailhog/MailHog/releases/download/v0.2.1/MailHog_linux_amd64
+chmod +x ~/mailhog
+mv ~/mailhog /usr/local/bin/mailhog
+
+# Install mhsendmail
+echo "Installing mhsendmail..."
+wget --quiet -O ~/mhsendmail https://github.com/mailhog/mhsendmail/releases/download/v0.2.0/mhsendmail_linux_amd64
+chmod +x ~/mhsendmail
+mv ~/mhsendmail /usr/local/bin/mhsendmail
+
+# Post installation cleanup
+echo "Cleaning up..."
+apt-get -y autoremove
+
+# nginx initial setup
+echo "Configuring nginx..."
+cp /srv/config/nginx/nginx.conf /etc/nginx/nginx.conf
+cp /srv/config/nginx/default.conf /etc/nginx/conf.d/default.conf
+cp /srv/config/nginx/mail.conf /etc/nginx/conf.d/mail.conf
+cp /srv/config/nginx/db.conf /etc/nginx/conf.d/db.conf
+sed -i "s/VAGRANT_DOMAIN/$vagrant_domain/g" /etc/nginx/conf.d/default.conf
+sed -i "s/VAGRANT_DOMAIN/$vagrant_domain/g" /etc/nginx/conf.d/mail.conf
+sed -i "s/VAGRANT_DOMAIN/$vagrant_domain/g" /etc/nginx/conf.d/db.conf
+
+# PHP initial setup
+echo "Configuring PHP..."
+phpenmod mbstring
+cp /srv/config/php/php-custom.ini /etc/php/7.4/fpm/conf.d/php-custom.ini
+sed -i "s/VAGRANT_DOMAIN/$vagrant_domain/g" /etc/php/7.4/fpm/conf.d/php-custom.ini
+
+# MySQL initial setup
+echo "Configuring MySQL..."
+mysql_secure_installation<<EOF
+password
+n
+Y
+Y
+Y
+Y
+EOF
+
+EXPECTED_ARGS=3
+MYSQL=`which mysql`
+#Q1="CREATE DATABASE IF NOT EXISTS $DBNAME;"
+Q2="GRANT ALL ON *.* TO '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q2}${Q3}"
+$MYSQL -uroot -p$DBROOTPASS -e "$SQL"
+
+# phpMyAdmin initial setup
+#echo "Configuring phpMyAdmin..."
+cp /srv/config/phpmyadmin/config.inc.php /etc/phpmyadmin/config.inc.php
+
+# Mailhog initial setup
+echo "Configuring Mailhog..."
+cp /srv/config/mailhog/mailhog.service  /etc/systemd/system/mailhog.service
+systemctl enable mailhog
+
+# Restart all the services
+echo "Restarting services..."
+service mysql restart
+service php7.4-fpm restart
+service nginx restart
+service mailhog start
+
+# Add ubuntu user to the www-data group with correct owner
+echo "Adding user to the correct group..."
+usermod -a -G www-data ubuntu
+mkdir -p /srv/www/html/wordpress
+chown -R www-data:www-data /srv/www/
+
+#checkout theme
+mkdir /srv/tmp
+cd /srv/tmp
+git clone https://$GIT_USER:"$GIT_PASS"@$GIT_REPO
+
+#install WP
+cd /srv/www/html/wordpress
+wp core download --version=$WP_VERSION --allow-root
+wp core config --dbprefix=$DBPREFIX --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
+wp db create --allow-root
+wp core install --url="$WPURL" --title="$WPSITENAME" --admin_user="$WPADMUSER" --admin_password="$WPADMPASS" --admin_email="$WPADMEMAIL" --allow-root
+sudo -u www-data  wp db import /srv/$DB_DUMP --dbuser=$DBUSER --dbpass=$DBPASS
+sudo -u www-data  wp option update home "$WPURL" 
+sudo -u www-data  wp option update siteurl "$WPURL" 
+sudo -u www-data  wp user update $WPADMUSER --user_pass="$WPADMPASS"
+
+#Install WP Plugins
+sudo -u www-data  wp plugin install contact-form-7 \
+                                    contact-form-7-dynamic-select-extension \
+                                    custom-post-type-ui \
+                                    cyr3lat \
+                                    easy-testimonials \
+                                    really-simple-ssl \
+                                    wpglobus \
+                                    advanced-custom-fields \
+                                    ga-google-analytics\
+                                    wonderm00ns-simple-facebook-open-graph-tags --activate
+
+#install theme
+cp -r /tmp/onix.kr.ua/wordpress/wp-content/themes/onix-ua /srv/www/html/wordpress/wp-content/themes/onix-ua
+cd /srv/www/html/wordpress/wp-content/themes/onix-ua
+npm install
+
+
+#Enable theme
+sudo -u www-data wp theme activate onix-ua
+
+# Calculate time taken and inform the user
+time_end="$(date +%s)"
+echo "Provisioning completed in "$(expr $time_end - $time_start)" seconds"
